d1076f06024209fd1e06537b410e36b9c2f7bae3
Abhyudaya Mittal <abhyudaya2018cs170abesit@gmail.com>
2022-09-07 17:16:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const socketServer = require("./socketServer");
 const authRoutes = require("./routes/authRoutes");
 const friendInvitationRoutes = require("./routes/friendInvitationRoutes");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());

d7f7bfb530cb065dfb393b0588dfd62235359ffc
Abhyudaya Mittal <abhyudaya2018cs170abesit@gmail.com>
2022-09-13 15:27:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 
 app.get('/',(req,res)=>{
 
-  res.send("Hello Chal gya bhai")
+  res.send("Server Running")
 
   res.end()
 
